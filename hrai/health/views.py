36e56362b22fd94207224e7d10b738a855a06d80
Valter Frank <glup0st1@mail.ru>
2023-10-14 07:01:38 +0300

    kekeke

--- hrai/health/views.py
+++ hrai/health/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+import requests
 
-# Create your views here.
+def get_russian_cities():
+    # Задайте ваш запрос Overpass. В этом примере мы ищем все объекты с тегом "place" равным "city" в России.
+    overpass_query = """
+    [out:json];
+    area[name="Россия"]->.a;
+    node["place"="city"](area.a);
+    out center;
+    """
+
+    # URL Overpass API
+    overpass_url = "https://overpass-api.de/api/interpreter"
+
+    # Отправьте запрос Overpass
+    response = requests.get(overpass_url, params={'data': overpass_query})
+
+    if response.status_code == 200:
+        data = response.json()
+        cities = set()
+
+        for element in data['elements']:
+            if 'name' in element['tags']:
+                cities.add(element['tags']['name'])
+
+        return list(cities)
+
+def indexpage(request):
+
+    cities = get_russian_cities()
+
+    return render(request, 'index.html', {'list_cities' : cities})
--- hrai/hrai/urls.py
+++ hrai/hrai/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from health.views import indexpage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', indexpage)
 ]
