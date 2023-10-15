24370aa9890f5d6aa1b0f0a74138b98a8f988b8d
Valter Frank <glup0st1@mail.ru>
2023-10-15 03:36:20 +0300

    favicon

--- hrai/health/views.py
+++ hrai/health/views.py
@@ -14,48 +14,54 @@ class JsonModelViewSet(viewsets.ModelViewSet):
 
 # Словарь с рейтингами объектов
 ratings = {
-    "e-cigarette": -40,
-    "tobacco": -90,
-    "bar": -60,
-    "biergarten": -50,
-    "pub": -50,
-    "wine": -30,
-    "alcohol": -80,
-    "beverages": -70,
-    "fast_food": -90,
-    "food_court": -80,
     "swimming pool": 90,
-    "stadium": 80,
-    "manege": 75,
+    "open. green area for recreation": 85,
+    "fresh fruit and vegetable stores": 85,
+    "farm shops": 85,
     "fitness club": 85,
-    "gym": 70,
+    "sports center": 85,
+    "equestrian tracks": 80,
+    "stadium": 80,
+    "parks": 80,
+    "football fields": 80,
+    "manege": 75,
     "bike path": 75,
     "skating rinks": 75,
     "ice fields for skating": 75,
-    "Parks": 80,
-    "open. green area for recreation": 85,
-    "pedestrian paths": 70,
-    "sidewalks": 60,
     "sports ground": 75,
-    "football fields": 80,
-    "sports center": 85,
-    "large stadiums": 70,
+    "pedestrian paths": 70,
+    "gym": 70,
     "jogging paths": 70,
+    "large stadiums": 70,
     "bicycle paths": 75,
-    "equestrian tracks": 80,
     "market": 65,
     "bazaar": 60,
-    "fresh fruit and vegetable stores": 85,
-    "farm shops": 85
+    "sidewalks": 60,
+    "wine": -30,
+    "bar": -60,
+    "pub": -50,
+    "biergarten": -50,
+    "beverages": -70,
+    "wine": -30,
+    "alcohol": -80,
+    "tobacco": -90,
+    "fast_food": -90,
+    "food_court": -80,
+    "e-cigarette": -40,
+    "weapons": -100,
+    "jewelry": -10
 }
+
 list_analysis = []
-# Функция для пересчета рейтинга в новый диапазон (1-100)
-def rescale_rating(old_rating):
-    old_min = -90
-    old_max = 90
-    new_min = 1
-    new_max = 100
-    return (old_rating - old_min) / (old_max - old_min) * (new_max - new_min) + new_min
+
+sorted_ratings = dict(sorted(ratings.items(), key=lambda x: x[1], reverse=True))
+
+def calculate_district_rating(positive_count, unhealthy_count):
+    if unhealthy_count >= 0:
+        rating = (positive_count / (positive_count + unhealthy_count)) * 100
+        return rating
+    else:
+        return 0
 
 # Функция для вычисления расстояния между двумя точками (широта и долгота) на поверхности Земли
 def haversine(lat1, lon1, lat2, lon2):
@@ -80,36 +86,71 @@ def haversine(lat1, lon1, lat2, lon2):
     distance = R * c
 
     return distance
-
 # Функция для обработки района
 def process_district(rel, api):
     if "name" in rel.tags:
         district_name = rel.tags["name"]
 
+        # Здесь можно добавить искусственную задержку, если количество районов больше 8
+
         # Запросы и обработка результатов как раньше
         query_education = f"""
         area[name="{district_name}"]->.district;
         (
-          node["amenity"="school"](area.district);
-          node["amenity"="university"](area.district);
-          node["amenity"="college"](area.district);
-          node["amenity"="kindergarten"](area.district);
-          node["amenity"="music_school"](area.district);
-          node["amenity"="technical school"](area.district);
-          node["amenity"="vocational lyceum"](area.district);
-          node["amenity"="secondary educational institution"](area.district);
-          node["amenity"="primary school"](area.district);
-          node["amenity"="secondary school"](area.district);
-          node["amenity"="gymnasium"](area.district);
-          node["amenity"="lyceum"](area.district);
-          node["amenity"="language school"](area.district);
-          node["amenity"="music school"](area.district);
+          node["education"="school"](area.district);
+          node["education"="university"](area.district);
+          node["education"="college"](area.district);
+          node["education"="kindergarten"](area.district);
+          node["education"="music_school"](area.district);
+          node["education"="technical school"](area.district);
+          node["education"="vocational lyceum"](area.district);
+          node["education"="secondary educational institution"](area.district);
+          node["education"="primary school"](area.district);
+          node["education"="secondary school"](area.district);
+          node["education"="gymnasium"](area.district);
+          node["education"="lyceum"](area.district);
+          node["education"="language school"](area.district);
+          node["education"="music school"](area.district);
         );
         out;
         """
         result_education = api.query(query_education)
         education_count = len(result_education.nodes)
-
+        positive_ratings = f"""
+        area[name="{district_name}"]->.district;
+        (
+          node["leisure"="park"](area.district);
+          node["amenity"="library"](area.district);
+          node["amenity"="theatre"](area.district);
+          node["amenity"="museum"](area.district);
+          node["amenity"="swimming pool"](area.district);
+          node["amenity"="stadium"](area.district);
+          node["leisure"="manege"](area.district);
+          node["leisure"="fitness club"](area.district);
+          node["leisure"="gym"](area.district);
+          node["leisure"="bike path"](area.district);
+          node["leisure"="skating rinks"](area.district);
+          node["leisure"="ice fields for skating"](area.district);
+          node["leisure"="parks"](area.district);
+          node["leisure"="green area for recreation"](area.district);
+          node["leisure"="pedestrian paths"](area.district);
+          node["leisure"="sidewalks"](area.district);
+          node["leisure"="sports ground"](area.district);
+          node["leisure"="football fields"](area.district);
+          node["leisure"="sports center"](area.district);
+          node["leisure"="large stadiums"](area.district);
+          node["leisure"="jogging paths"](area.district);
+          node["leisure"="bicycle paths"](area.district);
+          node["leisure"="equestrian tracks"](area.district);
+          node["shop"="market"](area.district);
+          node["shop"="bazaar"](area.district);
+          node["shop"="fresh fruit and vegetable stores"](area.district);
+          node["shop"="farm shops"](area.district);
+        );
+        out;
+        """
+        result_positive = api.query(positive_ratings)
+        positive_count = len(result_positive.nodes)
         # Запросы для негативно влияющих и позитивно влияющих объектов в районе
         query_unhealthy = f"""
         area[name="{district_name}"]->.district;
@@ -117,8 +158,6 @@ def process_district(rel, api):
           node["shop"="tobacco"](area.district);
           node["shop"="alcohol"](area.district);
           node["shop"="fast_food"](area.district);
-          node["shop"="jewelry"](area.district);
-          node["shop"="weapons"](area.district);
           node["shop"="e-cigarette"](area.district);
           node["shop"="biergarten"](area.district);
           node["shop"="pub"](area.district);
@@ -131,54 +170,39 @@ def process_district(rel, api):
         result_unhealthy = api.query(query_unhealthy)
         unhealthy_count = len(result_unhealthy.nodes)
 
-        query_positive = f"""
-        area[name="{district_name}"]->.district;
-        (
-          node["amenity"="park"](area.district);
-          node["amenity"="library"](area.district);
-          node["amenity"="theatre"](area.district);
-          node["amenity"="museum"](area.district);
-        );
-        out;
-        """
-        result_positive = api.query(query_positive)
-        positive_count = len(result_positive.nodes)
-
         # Создаем словарь для подсчета количества объектов для каждого тега
-        tag_counts = {}
+        tag_counts_positive = {}
+        tag_counts_negative = {}
+        # Для негативных тегов
 
-        # Считаем объекты для тегов, негативно влияющих на район
         for node in result_unhealthy.nodes:
             node_tag = node.tags.get("shop", "")
             if node_tag in ratings:
-                if node_tag in tag_counts:
-                    tag_counts[node_tag] += 1
+                if node_tag in tag_counts_negative:
+                    tag_counts_negative[node_tag] += 1
                 else:
-                    tag_counts[node_tag] = 1
+                    tag_counts_negative[node_tag] = 1
 
-        # Считаем объекты для тегов, позитивно влияющих на район
+        # Для положительных тегов
         for node in result_positive.nodes:
-            node_tag = node.tags.get("amenity", "")
-            if node_tag in ratings:
-                if node_tag in tag_counts:
-                    tag_counts[node_tag] += 1
+            node_tag = node.tags.get("amenity", "") or node.tags.get("shop", "") or node.tags.get("leisure", "")
+            if node_tag in positive_ratings:
+                if node_tag in tag_counts_positive:
+                    tag_counts_positive[node_tag] += 1
                 else:
-                    tag_counts[node_tag] = 1
-
-        # Вычисляем рейтинг района как отношение положительных к отрицательным объектам и пересчитываем его
-        if unhealthy_count >= 0:
-            district_rating = (positive_count / (unhealthy_count + positive_count)) * 100
-        # else:
-        #     district_rating = 0
+                    tag_counts_positive[node_tag] = 1
 
         # Выводим информацию о районе и его рейтинге
+        district_rating = calculate_district_rating(positive_count, unhealthy_count)
+
         analysis = {'district': district_name,
-                    'educations' : education_count,
-                    'unhealthy' : unhealthy_count,
-                    'positive' : positive_count,
-                    'rating' : district_rating,
+                    'educations': education_count,
+                    'unhealthy': unhealthy_count,
+                    'positive': positive_count,
+                    'rating': district_rating,
                     }
-        analysis.update(tag_counts)
+        analysis.update(tag_counts_negative)
+        analysis.update(tag_counts_positive)
         analysis['ecigarette'] = analysis.pop('e-cigarette')
         list_analysis.append(analysis)
 
@@ -191,7 +215,7 @@ def get_info(city_name):
     query_districts_9 = f"""
     area[name="{city_name}"]->.city;
     (
-    relation(area.city)["boundary"="administrative"]["admin_level"="9"];
+      relation(area.city)["boundary"="administrative"]["admin_level"="9"];
     );
     out;
     """
@@ -199,18 +223,33 @@ def get_info(city_name):
     # Выполните запрос для районов с admin_level=9
     result_districts_9 = api.query(query_districts_9)
 
-    # Если районов с admin_level=9 нет, используйте admin_level=8
-    if not result_districts_9.relations:
-        query_districts_8 = f"""
+    # Запрос для получения границ районов города (admin_level 8)
+    query_districts_8 = f"""
+    area[name="{city_name}"]->.city;
+    (
+      relation(area.city)["boundary"="administrative"]["admin_level"="8"];
+    );
+    out;
+    """
+
+    # Выполните запрос для районов с admin_level=8
+    result_districts_8 = api.query(query_districts_8)
+
+    # Используйте результат с наивысшим доступным уровнем (9, если 8 тоже есть)
+    if result_districts_9.relations:
+        result_districts = result_districts_9
+    elif result_districts_8.relations:
+        result_districts = result_districts_8
+    else:
+        query_city = f"""
         area[name="{city_name}"]->.city;
         (
-        relation(area.city)["boundary"="administrative"]["admin_level"="8"];
+        relation(area.city)["admin_level"];
         );
         out;
         """
-        result_districts = api.query(query_districts_8)
-    else:
-        result_districts = result_districts_9
+        result_city = api.query(query_city)
+        result_districts = result_city
 
     # Создайте список потоков для обработки районов
     threads = []
@@ -218,39 +257,10 @@ def get_info(city_name):
         thread = threading.Thread(target=process_district, args=(rel, api))
         threads.append(thread)
         thread.start()
-        time.sleep(0.7)
+        time.sleep(0.8)
 
     for thread in threads:
         thread.join()
-
-# Функция для получения всех Российских городов
-# def get_russian_cities():
-#     # Данные для запроса
-#     overpass_query = """
-#     [out:json];
-#     area[name="Россия"]->.a;
-#     (
-#         node["place"="city"](area.a);
-#         way["place"="city"](area.a);
-#         relation["place"="city"](area.a);
-#     );
-#     out center;
-#     """
-#     # Url api для запроса
-#     overpass_url = "https://overpass-api.de/api/interpreter"
-#     # Запрос
-#     response = requests.get(overpass_url, params={'data': overpass_query})
-#     # Проверка кода ответа & создание массива городов
-#     if response.status_code == 200:
-#         data = response.json()
-#         cities = []
-
-#         for element in data['elements']:
-#             if 'name' in element['tags'] and element['tags']['name'] not in cities:
-#                 cities.append(element['tags']['name'])
-
-#         return cities
-
 
 def get_self_api():
     cities = []
